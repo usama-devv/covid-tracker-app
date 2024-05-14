9a5b3e595a2a2a8841493862a787e1d4e667e127
Usama Masood <134695768+usama-devv@users.noreply.github.com>
2024-05-14 22:26:47 +0500

    added comments

--- lib/main.dart
+++ lib/main.dart
@@ -12,6 +12,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      // This is title widget
       title: 'COVID-19 Tracker',
       debugShowCheckedModeBanner: false,
       theme: ThemeData(
